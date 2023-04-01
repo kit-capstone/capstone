6809e01893240b4c0507ab0df7b084c5c2745c2a
jonghyeon <kiri6077@naver.com>
2023-04-01 16:38:43 +0900

    Kakao SDK 연결 완료

--- app/src/main/java/com/example/banlancegameex/MainActivity.kt
+++ app/src/main/java/com/example/banlancegameex/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.banlancegameex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.kakao.sdk.common.util.Utility
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val keyHash = Utility.getKeyHash(this)
+        Log.d("keyHash", keyHash)
     }
 }
