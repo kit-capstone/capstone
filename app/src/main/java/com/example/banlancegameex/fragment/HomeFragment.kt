6c53cd41e375ab1e429d53816db6c2fbe2bb7261
jonghyeon <kiri6077@naver.com>
2023-04-17 21:04:43 +0900

    HomeFragment 주석 달기 완료

--- app/src/main/java/com/example/banlancegameex/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/banlancegameex/fragment/HomeFragment.kt
@@ -27,10 +27,14 @@ import kotlin.random.Random
 class HomeFragment : Fragment() {
 
     private lateinit var binding : FragmentHomeBinding
+    // 사용자의 uid를 가져오기 위한 FirebaseAuth
     private lateinit var auth : FirebaseAuth
+    // firebase DB에 사용자 정보를 저장할 위치
     lateinit var postRef : DatabaseReference
+    // 사용자가 북마크한 게임의 key값을 저장할 list
     val bookmarkIdList = mutableListOf<String>()
 
+    // recycler view를 위한 adapter
     lateinit var rvAdapter : ContentRVAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,6 +56,7 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentHomeBinding.inflate(layoutInflater)
 
+        // FirebaseAuth 초기화
         auth = FirebaseAuth.getInstance()
 
         // 게시물을 가져올 ContentModel형태의 list
@@ -59,25 +64,37 @@ class HomeFragment : Fragment() {
         // ContentModel형태로 가져온 게시물들을 key 값을 저장할 list
         val itemKeyList = ArrayList<String>()
 
+        // 커스텀 adaper를 불러옴
         rvAdapter = ContentRVAdapter(requireContext(), items, itemKeyList, bookmarkIdList)
 
         val database = Firebase.database
 
         postRef = database.getReference("post")
 
+        // post 테이블에서 inquiry(조회수) 속성을 기준으로 오름차순으로 게임 정보를 받아옴
         postRef.orderByChild("inquiry").addValueEventListener(object : ValueEventListener{
             override fun onDataChange(snapshot: DataSnapshot) {
 
                 for(dataModel in snapshot.children){
+                    // 게임 하나의 정보를 items에 push
                     val item = dataModel.getValue(ContentModel::class.java)
                     items.add(item!!)
+
+                    // push한 게임의 key 값을 itemKeyList에 저장
+                    // 사용자가 북마크한 게임을 식별하기 위한 변수
                     itemKeyList.add(dataModel.key.toString())
                 }
+
+                // 가져온 게임 정보들을 내림차순으로 정렬
+                // inquiry(조회수)가 큰 게임부터 출력하기 위한 코드
                 items.reverse()
                 itemKeyList.reverse()
 
+                // 비동기 방식으로 받아온 정보를 recycler view에 반영하기 위한 코드
                 rvAdapter.notifyDataSetChanged()
 
+                // 오늘의 추천으로 출력할 게임을 선정하기 위한 코드
+                // 위에서 받아온 게임 정보 중 상위 3개 중 임의로 선정
                 val subList = items.subList(0, 3)
                 val randomIndex = Random.nextInt(subList.size)
                 val recommedElement = subList[randomIndex]
@@ -92,12 +109,15 @@ class HomeFragment : Fragment() {
             }
         })
 
+        // recycler view 불러옴
         val rv : RecyclerView = binding.contentsRV
 
+        // recycler view와 adapter 연결
         rv.adapter = rvAdapter
 
         rv.layoutManager = GridLayoutManager(requireContext(), 1)
 
+        // 북마크한 게임 정보를 불러오기 위한 함수
         getBookmarkData()
 
 
@@ -123,6 +143,8 @@ class HomeFragment : Fragment() {
 
     private fun getBookmarkData() {
         val bookmarkRef : DatabaseReference
+        // 사용자의 uid를 객체의 key값으로 지정
+        // 각 객체에 북마크한 게임 정보의 key값을 bookmark_list 테이블에 저장, 불러오는 코드
         bookmarkRef = Firebase.database.getReference("bookmark_list")
 
         val postListener = object : ValueEventListener {
