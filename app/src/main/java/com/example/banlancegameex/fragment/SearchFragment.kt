807355929a9c4410e7e541a8eb5ad55f8b223442
jonghyeon <kiri6077@naver.com>
2023-04-24 20:22:03 +0900

    게시물 검색 태그 기능 구현

--- app/src/main/java/com/example/banlancegameex/fragment/SearchFragment.kt
+++ app/src/main/java/com/example/banlancegameex/fragment/SearchFragment.kt
@@ -37,6 +37,11 @@ class SearchFragment : Fragment() {
 
     var tagList : ArrayList<String> = arrayListOf()
 
+    // 게시물을 가져올 ContentModel형태의 list
+    val items = ArrayList<ContentModel>()
+    // ContentModel형태로 가져온 게시물들을 key 값을 저장할 list
+    val itemKeyList = ArrayList<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -55,48 +60,145 @@ class SearchFragment : Fragment() {
         binding = FragmentSearchBinding.inflate(layoutInflater)
 
 
-        // 게시물을 가져올 ContentModel형태의 list
-        val items = ArrayList<ContentModel>()
-        // ContentModel형태로 가져온 게시물들을 key 값을 저장할 list
-        val itemKeyList = ArrayList<String>()
-
         val itemssearch = ArrayList<SearchContentModel>()
 
-        // items를 tag 기준으로 화면에 띄울 게시물 list
-        val viewitems = ArrayList<ContentModel>()
-        // viewitems의 게시물들의 key 값을 저장할 list
-        val viewitemKeyList = ArrayList<String>()
+        var search_filter : List<SearchContentModel> = listOf()
 
-        binding.tagCheckedBtn.setOnClickListener{
-            tagList.clear()
-            getTagList()
+        binding.loveBtn.setOnClickListener{
+            if(binding.loveBtn.isChecked){
+                tagList.add("연애")
+            } else {
+                tagList.remove("연애")
+            }
+            items.clear()
+            itemKeyList.clear()
+
+            search_title = binding.searchText.text.toString()
+            if(search_title == ""){
+                setTagPost(itemssearch)
+            }else {
+                setTagPost(search_filter)
+            }
+
+            rvAdapter.notifyDataSetChanged()
+        }
+
+        binding.gameBtn.setOnClickListener{
+            if(binding.gameBtn.isChecked){
+                tagList.add("게임")
+            } else {
+                tagList.remove("게임")
+            }
+            items.clear()
+            itemKeyList.clear()
+
+            search_title = binding.searchText.text.toString()
+            if(search_title == ""){
+                setTagPost(itemssearch)
+            }else {
+                setTagPost(search_filter)
+            }
+
+            rvAdapter.notifyDataSetChanged()
+        }
+
+        binding.foodBtn.setOnClickListener{
+            if(binding.foodBtn.isChecked){
+                tagList.add("음식")
+            } else {
+                tagList.remove("음식")
+            }
+            items.clear()
+            itemKeyList.clear()
+
+            search_title = binding.searchText.text.toString()
+            if(search_title == ""){
+                setTagPost(itemssearch)
+            }else {
+                setTagPost(search_filter)
+            }
+
+            rvAdapter.notifyDataSetChanged()
+        }
+
+        binding.favoriteBtn.setOnClickListener{
+            if(binding.favoriteBtn.isChecked){
+                tagList.add("취미")
+            } else {
+                tagList.remove("취미")
+            }
+            items.clear()
+            itemKeyList.clear()
+
+            search_title = binding.searchText.text.toString()
+            if(search_title == ""){
+                setTagPost(itemssearch)
+            }else {
+                setTagPost(search_filter)
+            }
+
+            rvAdapter.notifyDataSetChanged()
+        }
+
+        binding.aniBtn.setOnClickListener{
+            if(binding.aniBtn.isChecked){
+                tagList.add("만화")
+            } else {
+                tagList.remove("운동")
+            }
+            items.clear()
+            itemKeyList.clear()
+
+            search_title = binding.searchText.text.toString()
+            if(search_title == ""){
+                setTagPost(itemssearch)
+            }else {
+                setTagPost(search_filter)
+            }
+
+            rvAdapter.notifyDataSetChanged()
+        }
+
+        binding.exerciseBtn.setOnClickListener{
+            if(binding.exerciseBtn.isChecked){
+                tagList.add("운동")
+            } else {
+                tagList.remove("운동")
+            }
+            items.clear()
+            itemKeyList.clear()
+
+            search_title = binding.searchText.text.toString()
+            if(search_title == ""){
+                setTagPost(itemssearch)
+            }else {
+                setTagPost(search_filter)
+            }
+
+            rvAdapter.notifyDataSetChanged()
         }
 
         binding.searchBtn.setOnClickListener {
             search_title = binding.searchText.text.toString()
             if(search_title != ""){
-                val search_filter = itemssearch.filter { it.title.contains(search_title) }
+                search_filter = itemssearch.filter { it.title.contains(search_title) }
                 items.clear()
                 itemKeyList.clear()
-//                Log.d("검색 테스트", search_filter.toString())
+                toggleInit()
+
                 for(item in search_filter) {
-//                        if(tagList != null){
-//                            for(tagitem in tagList){
-//                                if(item.tag.contains(tagitem)){
-//                                    items.add(ContentModel(item.title, item.option1, item.option1Sub, item.option2, item.option2Sub,
-//                                    item.inquiry, item.tag, item.locate, item.uid, item.time))
-//                                    itemKeyList.add(item.key)
-//                                }
-//                            }
-//                        } else {
-//                            items.add(ContentModel(item.title, item.option1, item.option1Sub, item.option2, item.option2Sub,
-//                                item.inquiry, item.tag, item.locate, item.uid, item.time))
-//                            itemKeyList.add(item.key)
-//                        }
                     items.add(ContentModel(item.title, item.option1, item.option1Sub, item.option2, item.option2Sub,
                         item.inquiry, item.tag, item.locate, item.uid, item.time))
                     itemKeyList.add(item.key)
 
+                }
+            } else {
+                items.clear()
+                itemKeyList.clear()
+                for(item in itemssearch){
+                    items.add(ContentModel(item.title, item.option1, item.option1Sub, item.option2, item.option2Sub,
+                        item.inquiry, item.tag, item.locate, item.uid, item.time))
+                    itemKeyList.add(item.key)
                 }
             }
             rvAdapter.notifyDataSetChanged()
@@ -111,14 +213,12 @@ class SearchFragment : Fragment() {
                     val item = datasnapshot.getValue(ContentModel::class.java)
 
                     items.add(item!!)
-                    viewitems.add(item!!)
                     itemssearch.add(SearchContentModel(item.title, item.option1, item.option1Sub, item.option2, item.option2Sub, item.inquiry, item.tag,
                     item.locate, item.uid, item.time, datasnapshot.key.toString()))
 
                     // push한 게임의 key 값을 itemKeyList에 저장
                     // 사용자가 북마크한 게임을 식별하기 위한 변수
                     itemKeyList.add(datasnapshot.key.toString())
-                    viewitemKeyList.add(datasnapshot.key.toString())
                 }
 
                 // 가져온 게임 정보들을 내림차순으로 정렬
@@ -188,24 +288,33 @@ class SearchFragment : Fragment() {
         bookmarkRef.child(FBAuth.getuid()).addValueEventListener(postListener)
     }
 
-    private fun getTagList(){
-        if(binding.loveBtn.isChecked) {
-            tagList.add("연애")
-        }
-        if(binding.gameBtn.isChecked){
-            tagList.add("게임")
-        }
-        if(binding.foodBtn.isChecked){
-            tagList.add("음식")
-        }
-        if(binding.favoriteBtn.isChecked){
-            tagList.add("취향")
-        }
-        if(binding.aniBtn.isChecked){
-            tagList.add("만화")
-        }
-        if(binding.exerciseBtn.isChecked){
-            tagList.add("운동")
+    private fun toggleInit(){
+        tagList.clear()
+        binding.loveBtn.isChecked = false
+        binding.gameBtn.isChecked = false
+        binding.foodBtn.isChecked = false
+        binding.favoriteBtn.isChecked = false
+        binding.aniBtn.isChecked = false
+        binding.exerciseBtn.isChecked = false
+    }
+
+    private fun setTagPost(postList : List<SearchContentModel>) {
+        if(tagList.isEmpty()) {
+            for(item in postList){
+                items.add(ContentModel(item.title, item.option1, item.option1Sub, item.option2, item.option2Sub,
+                    item.inquiry, item.tag, item.locate, item.uid, item.time))
+                itemKeyList.add(item.key)
+            }
+        } else {
+            for(tag in tagList) {
+                for(item in postList){
+                    if(item.tag.contains(tag)){
+                        items.add(ContentModel(item.title, item.option1, item.option1Sub, item.option2, item.option2Sub,
+                            item.inquiry, item.tag, item.locate, item.uid, item.time))
+                        itemKeyList.add(item.key)
+                    }
+                }
+            }
         }
     }
 
